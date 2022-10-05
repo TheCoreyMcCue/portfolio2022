c08d9e997e186c5e0621a7304bb82f05207e9e49
Corey McCue <coreysmccue@gmail.com>
2022-10-05 14:45:29 +0200

    initial header styling and add framer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
+import { SocialIcon } from 'react-social-icons';
+
 type Props = {};
 
 function Header({}: Props) {
 	return (
-		<header>
-			<div></div>
+		<header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+			<div className='flex flex-row items-center'>
+				<SocialIcon
+					className='mx-2'
+					target='_blank'
+					// fgColor='white'
+					// bgColor='gray'
+					url='https://www.linkedin.com/in/corey-mccue-80949875/'
+				/>
+				<SocialIcon
+					className='mx-2'
+					target='_blank'
+					// fgColor='white'
+					// bgColor='gray'
+					url='https://www.linkedin.com/in/corey-mccue-80949875/'
+				/>
+				<SocialIcon
+					className='mx-2'
+					target='_blank'
+					// fgColor='white'
+					// bgColor='gray'
+					url='https://www.linkedin.com/in/corey-mccue-80949875/'
+				/>
+			</div>
+			<div className='flex flex-row items-center'>
+				<SocialIcon
+					fgColor='gray'
+					bgColor='transparent'
+					className='cursor-pointer mx-2'
+					network='email'
+				/>
+				<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+					Get in Touch
+				</p>
+			</div>
 		</header>
 	);
 }
